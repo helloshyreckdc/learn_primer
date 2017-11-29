23b5598fe87fa3a694e3316ab7d686234b8e1bd1
helloshyreckdc <shyreckdc@gmail.com>
2017-11-30 00:30:18 +0800

    chapter1 finished

--- 1/prog1.cc
+++ 1/prog1.cc
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-	cout << "/*" << endl;
-	cout << "*/" << endl;
-//  This is wrong
-//	cout << /* "*/" */ << endl; 
-	cout << /* "*/" /* "/*" */ << endl;
+/* test cout, cin, cerr, clog */
+//int main()
+//{
 //	cout << "Enter two numbers:" << endl;
 //	int v1, v2;
 //	cin >> v1 >> v2;
 //	cout << "Sum is " << v1 + v2 << endl;
 //	return 0;
+//}
+
+/* test comment */
+//int main()
+//{
+//	cout << "/*" << endl;
+//	cout << "*/" << endl;
+////  This is wrong
+////	cout << /* "*/" */ << endl; 
+//	cout << /* "*/" /* "/*" */ << endl;
+//}
+
+/* test uncertain numbers */
+int main()
+{
+	int value, sum = 0;
+	cout << "Please input some integers:" << endl;
+	while(cin >> value)
+	{
+		sum += value;
+	}
+	cout << "sum is " << sum << endl;
 }
